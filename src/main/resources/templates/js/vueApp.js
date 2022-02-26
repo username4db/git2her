3a73a1bb57d99368d42ff6241faba105f6a83aa3
user <user@192.168.0.48>
2022-02-26 16:39:34 +0800

    motp

---
+++ src/main/resources/templates/js/vueApp.js
@@ -0,0 +1,20 @@
+const vueApp = Vue.createApp({
+	data() {
+		return {
+			show: true,
+			items:[
+				{key:'1',value:'A'}
+			]
+		}
+	},
+	methods: {
+		toggle() {
+			this.show = !this.show
+		}
+	},
+	computed: {
+		filtered() {
+			return this.items.filter((item) => return true})
+		}
+	}
+})
