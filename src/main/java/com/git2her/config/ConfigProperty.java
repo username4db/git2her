a2eaabfba4f24bd6d0918ad4861883342065ef02
Administrator <Administrator@192.168.0.48>
2022-05-28 09:33:29 +0800

    config

--- src/main/java/com/git2her/config/ConfigProperty.java
+++ src/main/java/com/git2her/config/ConfigProperty.java
@@ -25,6 +25,30 @@ public class ConfigProperty {
 			return "Credentials [authMethod=" + authMethod + ", username=" + username + ", password=" + password + "]";
 		}
 
+		public String getAuthMethod() {
+			return authMethod;
+		}
+
+		public void setAuthMethod(String authMethod) {
+			this.authMethod = authMethod;
+		}
+
+		public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+
 	}
 
 	private String host;
@@ -38,6 +62,54 @@ public class ConfigProperty {
 	public String toString() {
 		return "ConfigProperty [host=" + host + ", port=" + port + ", from=" + from + ", defaultRecipients="
 				+ defaultRecipients + ", additionalHeaders=" + additionalHeaders + ", credentials=" + credentials + "]";
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public List<String> getDefaultRecipients() {
+		return defaultRecipients;
+	}
+
+	public void setDefaultRecipients(List<String> defaultRecipients) {
+		this.defaultRecipients = defaultRecipients;
+	}
+
+	public Map<String, String> getAdditionalHeaders() {
+		return additionalHeaders;
+	}
+
+	public void setAdditionalHeaders(Map<String, String> additionalHeaders) {
+		this.additionalHeaders = additionalHeaders;
+	}
+
+	public Credentials getCredentials() {
+		return credentials;
+	}
+
+	public void setCredentials(Credentials credentials) {
+		this.credentials = credentials;
 	}
 
 }
